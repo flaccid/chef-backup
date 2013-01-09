74b477eccabc73ecd57aed4bd6c1927df31d1f6a
Chris Fordham <chris@xhost.com.au>
2013-01-09 15:07:31 +1100

    Improve default recipe to not assume /home for backup_user, deps install and template configuration, using aws node attributes for aws credentials.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,54 +7,54 @@
 # MIT license
 #
 
+include_recipe "aws"
+
+# in theory, only make will be needed for some of the gem installs (further below)
+#include_recipe "build-essential"
+
+# pre-install dep packages for gems
+p = package "make" do
+  action :nothing
+end
+p.run_action(:install)
+
+p = package "libxslt-dev" do
+  action :nothing
+end
+p.run_action(:install)
+
+p = package "libxml2-dev" do
+  action :nothing
+end
+p.run_action(:install)
+
 # Install backup, s3sync, fog, mail, whenever
-
-package "libxslt" do
-  package_name "libxslt-dev"
-  action :install
-end
-
-package "libxml-dev" do
-  package_name "libxml2-dev"
-  action :install
-end
-
-
 ['backup', 's3sync', 'fog', 'mail', 'whenever', 'popen4'].each do |gem_name|
-  gem_package gem_name do
-    action :install
-  end
+ chef_gem gem_name
 end
 
+# create Backup directories
 ['Backup', 'Backup/config'].each do |dir|
-  execute "mkdir /home/#{node[:backup][:backup_user]}/#{dir}" do
-    user node[:backup][:backup_user]
-    only_if { !File.directory?("/home/#{node[:backup][:backup_user]}/#{dir}") }
+  directory "#{`echo ~#{node['backup']['user']}`.strip}/#{dir}" do
+    owner node['backup']['user']
   end
 end
 
-template "/home/#{node[:backup][:backup_user]}/Backup/config.rb" do
-  owner node[:backup][:backup_user]
+template "#{`echo ~#{node['backup']['user']}`.strip}/Backup/config.rb" do
+  owner node['backup']['user']
   source "config.rb.erb"
   variables(:config => node[:backup])
-  not_if { File.exists?("/home/#{node[:backup][:backup_user]}/Backup/config.rb")}
 end
 
-
 # Whenever config setup.
-template "/home/#{node[:backup][:backup_user]}/Backup/config/schedule.rb" do
-  owner node[:backup][:backup_user]
+template "#{`echo ~#{node['backup']['user']}`.strip}/Backup/config/schedule.rb" do
+  owner node['backup']['user']
   source "schedule.rb.erb"
   variables(:config => node[:backup])
-  not_if { File.exists?("/home/#{node[:backup][:backup_user]}/Backup/config/schedule.rb")}
 end
 
 template "/etc/logrotate.d/whenever_log" do
   owner "root"
   source "logrotate.erb"
-  variables(:backup_path => "/home/#{node[:backup][:backup_user]}/Backup")
-  not_if { File.exists? "/etc/logrotate.d/whenever_log" }
+  variables(:backup_path => "#{`echo ~#{node['backup']['user']}`.strip}/Backup")
 end
-
-
-
